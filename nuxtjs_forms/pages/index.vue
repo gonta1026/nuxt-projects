bc4a6f911857b3e1a21c2211513e3693c06dbb9b
gonta1026 <katano.original@gmail.com>
2020-05-27 23:50:25 +0900

    fix

--- nuxtjs_forms/pages/index.vue
+++ nuxtjs_forms/pages/index.vue
@@ -7,7 +7,7 @@
       <div class="form-group">
         <label>性別</label>
         <div>
-          <label class="form-check form-check-inline">
+          <label class="form-check form-check-inline is-invalid">
             <input class="form-check-input" type="radio" value="male" v-model="$v.form.gender.$model">
             男性
           </label>
@@ -15,11 +15,21 @@
             <input class="form-check-input" type="radio" value="female" v-model="$v.form.gender.$model">
             女性
           </label>
+          <div class="text-danger" v-if="$v.form.gender.$error">
+            性別を選択してください。
+          </div>
         </div>
+
       </div>
       <div class="form-group">
         <label>件名</label>
         <input type="text" class="form-control" aria-label="件名" v-model="$v.form.title.$model">
+        <div class="text-danger" v-if="$v.form.title.$error && !$v.form.title.required">
+          件名を入力してください。
+        </div>
+        <div class="text-danger" v-if="$v.form.title.$error && !$v.form.title.maxLength">
+          件名は200文字以内で入力してください。
+        </div>
       </div>
       <div class="form-group">
         <label>部署</label>
@@ -29,14 +39,26 @@
           <option value="sales">営業部</option>
           <option value="hanbai">販売部</option>
         </select>
+        <div class="text-danger" v-if="$v.form.subject.$error">
+          部署を選択してください。
+        </div>
       </div>
       <div class="form-group">
         <label>本文</label>
         <textarea type="text" class="form-control" rows="5" aria-label="本文" v-model="$v.form.body.$model"/>
+        <div class="text-danger" v-if="!$v.form.body.required && $v.form.body.$error">
+          本文を入力してください。
+        </div>
+        <div class="text-danger" v-if="!$v.form.body.minLength">
+          本文は100文字以上で入力してください。
+        </div>
       </div>
       <label class="form-group form-check">
         <input type="checkbox" class="form-check-input" v-model="$v.form.check.$model">
         個人情報の取扱に同意します。
+        <div class="text-danger" v-if="$v.form.check.$error">
+          個人情報の取扱に同意してください。
+        </div>
       </label>
       <button type="submit" class="btn btn-primary" @click.prevent="submit">送信</button>
     </form>
@@ -45,8 +67,9 @@
 
 <script>
 import {required,maxLength,minLength} from "vuelidate/lib/validators"
-const is = (param) => value => value === param;
 
+
+const is = (param) => value => value === param;// ルールを追加
 export default {
   data(){
     return {
@@ -61,6 +84,7 @@ export default {
   },
   methods:{
     submit(){
+      this.$v.$touch()
       if(this.$v.$invalid){
         console.log("バリデーションエラー")
       }else{
