d123b83b62c411ecdb53ca8ce1f38f4ad9b81fe5
gonta1026 <katano.original@gmail.com>
2020-06-06 10:19:46 +0900

    fix

---
+++ my-first-nuxt-app/plugins/axios.js
@@ -0,0 +1,10 @@
+console.log(process.env.QIITA_TOKEN)
+export default function ({ $axios }) {
+  $axios.onRequest((config) => {
+    if (process.env.QIITA_TOKEN) {
+      // 実際の開発の場合は store.state.token などを参照することになるはずです
+      config.headers.common['Authorization'] = `Bearer ${process.env.QIITA_TOKEN}`
+    }
+    return config
+  })
+}
