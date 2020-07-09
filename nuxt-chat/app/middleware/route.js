7e4ea8762796ee4e51b5f17a05c478a9d927e5aa
gonta1026 <katano.original@gmail.com>
2020-07-10 01:44:50 +0900

    fix

--- nuxt-chat/app/middleware/route.js
+++ nuxt-chat/app/middleware/route.js
@@ -2,7 +2,7 @@ import firebase from "~/plugins/firebase";
 import Cookies from "universal-cookie"
 const cookies = new Cookies();
 const loginState = cookies.get('login_now')
-console.log("ss",loginState)
+// console.log("ss",loginState)
 // const loginState = cookies.get('login_now') ? cookies.get('login_now') : null
 export default ({ req, route, redirect, store }) => {
   // firebase.auth().onAuthStateChanged(function (user) {
@@ -12,16 +12,18 @@ export default ({ req, route, redirect, store }) => {
       if (route.path === "/login/" || route.path === "/signup/") {
         return redirect("/group/");
       }
+      console.log("ログインしている",loginState)
     } else {
+      console.log("ログインしていない", loginState)
       //!!ここのコメントアウトを外すとエラーになる。
       /* ログインしていないのに "ログインと新規登録" 以外のページに行ったらloginページに戻す*/
-      // if (route.path !== "/login/" || route.path !== "/signup/") {
-      //   return redirect("/login/");
-      // }
+      if (route.path !== "/login/" || route.path !== "/signup/") {
+        // return redirect("/login/");
+      }
     }
-    // if (route.path === "/") { //ルートで来た際は処理をスキップ
-    //   return redirect('/group/')
-    // }
+    if (route.path === "/") {
+      return redirect('/group/')
+    }
 
   // //!! ここでリダイレクトの指示を出してもうまくリダイレクトされない？
   // /* ログインしているのに "/login" のパスを使ったらルートに戻す*/
